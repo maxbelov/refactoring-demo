6d8706f5c0b384fb16b134b928c932fed1e79e45
askryl <askryl@jet.msk.su>
2016-02-29 15:09:50 +0300

    Customer test rev 2

--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -1,5 +1,102 @@
 /**
  * Created by askryl on 29.02.16.
  */
+
+import com.scrumtrek.simplestore.Customer;
+import com.scrumtrek.simplestore.Movie;
+import com.scrumtrek.simplestore.PriceCodes;
+import com.scrumtrek.simplestore.Rental;
+import jdk.nashorn.internal.ir.annotations.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+import static org.fest.assertions.Assertions.*;
+
 public class CustomerTest {
+
+    @Ignore
+    @Test
+    public void baseTest() {
+
+        Movie movCinderella = new Movie("Cinderella", PriceCodes.Childrens);
+        Movie movStarWars = new Movie("Star Wars", PriceCodes.Regular);
+        Movie movGladiator = new Movie("Gladiator", PriceCodes.NewRelease);
+
+        // Create customers
+        Customer custMickeyMouse = new Customer("Mickey Mouse");
+        Customer custDonaldDuck = new Customer("Donald Duck");
+        Customer custMinnieMouse = new Customer("Minnie Mouse");
+
+        // Create rentals
+        Rental rental1 = new Rental(movCinderella, 5);
+        Rental rental2 = new Rental(movStarWars, 5);
+        Rental rental3 = new Rental(movGladiator, 5);
+
+        // Assign rentals to customers
+        custMickeyMouse.addRental(rental1);
+        custMickeyMouse.addRental(rental2);
+        custMickeyMouse.addRental(rental3);
+
+        // Generate invoice
+        String statement = custMickeyMouse.Statement();
+
+        // Print the statement
+        //System.out.println(statement);
+    }
+
+    @Test
+    public void getReportOnRegularMovieOneDayOneCustomer() {
+
+        Movie testMovie = new Movie("Star Wars", PriceCodes.Regular);
+
+
+        Customer testCustomer = new Customer("Test Customer");
+
+        // Create rentals
+        Rental testRental = new Rental(testMovie, 1);
+
+        // Assign rentals to customers
+        testCustomer.addRental(testRental);
+
+        // Generate invoice
+        String statement = testCustomer.Statement();
+
+        // Print the statement
+        System.out.println(statement);
+        assertThat(statement).
+                contains("Rental record for Test Customer").
+                contains("Star Wars\t2.0").
+                contains("Amount owed is 2.0").
+                contains("You earned 1 frequent renter points");
+
+
+    }
+
+    @Test
+    public void getReportOnRegularMovieThreeDayOneCustomer() {
+
+        Movie testMovie = new Movie("Star Wars", PriceCodes.Regular);
+
+
+        Customer testCustomer = new Customer("Test Customer");
+
+        // Create rentals
+        Rental testRental = new Rental(testMovie, 3);
+
+        // Assign rentals to customers
+        testCustomer.addRental(testRental);
+
+        // Generate invoice
+        String statement = testCustomer.Statement();
+
+        // Print the statement
+        System.out.println(statement);
+        assertThat(statement).
+                contains("Rental record for Test Customer").
+                contains("Star Wars\t3.5").
+                contains("Amount owed is 3.5").
+                contains("You earned 1 frequent renter points");
+
+
+    }
 }
--- src/test/java/MovieTest.java
+++ src/test/java/MovieTest.java
@@ -1,5 +1,23 @@
 /**
  * Created by askryl on 29.02.16.
  */
+
+import com.scrumtrek.simplestore.Movie;
+import com.scrumtrek.simplestore.PriceCodes;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+
 public class MovieTest {
+    @Test
+    public void shouldReturnPriceCodeAndTitleAfterCreate() {
+
+        Movie sut = new Movie("Test1", PriceCodes.Childrens);
+
+        assertTrue( sut.getPriceCode() == PriceCodes.Childrens );
+        assertTrue( "Test1".equals( sut.getTitle() ) );
+
+
+    }
 }
